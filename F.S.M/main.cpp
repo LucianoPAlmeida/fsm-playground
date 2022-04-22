52aa4bbdb958485a3002de756b96f47a5268fa67
Luciano Almeida <lucianopa@microsoft.com>
2022-04-21 22:57:38 -0300

    Initial Commit

---
+++ F.S.M/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  F.S.M
+//
+//  Created by Luciano Almeida on 21/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
