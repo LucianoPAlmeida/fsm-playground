a87440cfe63eb39da126b4ee190f54c751017b46
Luciano Almeida <lucianopa@microsoft.com>
2022-04-28 00:51:25 -0300

    An even simpler Deterministic Machine

--- F.S.M/main.cpp
+++ F.S.M/main.cpp
@@ -81,20 +81,49 @@ static std::unique_ptr<Machine> make01s10sMachine() {
                                    /*startState=*/0);
 }
 
-int main(int argc, const char * argv[]) {
-  auto M = make01s10sMachine();
-  std::cout << std::boolalpha << "001: " << M->accept("001") << std::endl;
-  std::cout << std::boolalpha << "100: " << M->accept("100") << std::endl;
-  std::cout << std::boolalpha << "1010: " << M->accept("1010") << std::endl;
-  std::cout << std::boolalpha << "0110: " << M->accept("0110") << std::endl;
-  std::cout << std::boolalpha << "1: " << M->accept("1") << std::endl;
-  std::cout << std::boolalpha << "0: " << M->accept("0") << std::endl;
-  std::cout << std::boolalpha << "01: " << M->accept("01") << std::endl;
-  std::cout << std::boolalpha << "10: " << M->accept("10") << std::endl;
-  std::cout << std::boolalpha << "111110000: " << M->accept("111110000")
-            << std::endl;
-  std::cout << std::boolalpha << "000011111: " << M->accept("00001111")
-            << std::endl;
+// 01, 10, 001, 110 ... 111110000, 00001111
+// A finite state machine starts in arbitrary 1's and 0' and ends with one or
+// more zeros
+static std::unique_ptr<Machine> makeEndInZerosMachine() {
+  std::unordered_map<size_t, std::unordered_map<char, size_t>> machineStates;
+  machineStates.insert({0, {{'0', 1}, {'1', 0}}});
+  machineStates.insert({1, {{'0', 1}, {'1', 0}}});
+  std::unordered_set<size_t> finalStates = {1};
 
+  return std::make_unique<Machine>(machineStates, finalStates,
+                                   /*startState=*/0);
+}
+
+int main(int argc, const char * argv[]) {
+  {
+    auto M = make01s10sMachine();
+    std::cout << std::boolalpha << "001: " << M->accept("001") << std::endl;
+    std::cout << std::boolalpha << "100: " << M->accept("100") << std::endl;
+    std::cout << std::boolalpha << "1010: " << M->accept("1010") << std::endl;
+    std::cout << std::boolalpha << "0110: " << M->accept("0110") << std::endl;
+    std::cout << std::boolalpha << "1: " << M->accept("1") << std::endl;
+    std::cout << std::boolalpha << "0: " << M->accept("0") << std::endl;
+    std::cout << std::boolalpha << "01: " << M->accept("01") << std::endl;
+    std::cout << std::boolalpha << "10: " << M->accept("10") << std::endl;
+    std::cout << std::boolalpha << "111110000: " << M->accept("111110000")
+              << std::endl;
+    std::cout << std::boolalpha << "000011111: " << M->accept("00001111")
+              << std::endl;
+  }
+
+  {
+    std::cout << "Ends in zeros" << std::endl;
+    auto M = makeEndInZerosMachine();
+    std::cout << std::boolalpha << "001: " << M->accept("001") << std::endl;
+    std::cout << std::boolalpha << "100: " << M->accept("100") << std::endl;
+    std::cout << std::boolalpha << "1010: " << M->accept("1010") << std::endl;
+    std::cout << std::boolalpha << "0110: " << M->accept("0110") << std::endl;
+    std::cout << std::boolalpha << "101: " << M->accept("101") << std::endl;
+    std::cout << std::boolalpha << "1000100: " << M->accept("1000100")
+              << std::endl;
+    std::cout << std::boolalpha << "0000: " << M->accept("0000") << std::endl;
+    std::cout << std::boolalpha << "00001: " << M->accept("00001") << std::endl;
+    std::cout << std::boolalpha << "1111: " << M->accept("1111") << std::endl;
+  }
   return 0;
 }
